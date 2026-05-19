ed5ed55713ddf0a6f36371724184f36d79161fa5
AMT94-star <aslihan.taskin94@gmail.com>
2026-05-19 14:38:08 +0200

    updated pom, added extra validation in user, init commit for dto

---
+++ src/main/java/se/amt/webshopauthgroup7/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package se.amt.webshopauthgroup7.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String username;
+
+    @NotBlank
+    private String password;
+}
